26d04609d82eb4cba98a4006d11700525f60600f
Arturo Aliaga <jaav2801@gmail.com>
2025-11-25 17:55:21 -0400

    Add webPrefix configuration for production environment in routing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        webPrefix: env('APP_ENV') === 'production' ? 'inf513/grupo01sc/tecnoweb/public' : '',
         api: __DIR__.'/../routes/api.php',
         apiPrefix: env('APP_ENV') === 'production' ? 'inf513/grupo01sc/tecnoweb/public/api' : 'api',
         commands: __DIR__.'/../routes/console.php',
