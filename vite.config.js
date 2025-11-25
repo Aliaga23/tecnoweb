f9dac12f7e9398b7ab39b3d0624d309a55964394
Arturo Aliaga <jaav2801@gmail.com>
2025-11-25 17:54:37 -0400

    Update Vite configuration to set correct base path for production build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/inf513/grupo01sc/tecnoweb/public/' : '/',
+    base: mode === 'production' ? '/inf513/grupo01sc/tecnoweb/public/build/' : '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
